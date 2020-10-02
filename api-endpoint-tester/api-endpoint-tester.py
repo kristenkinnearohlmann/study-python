bc8cd6d08b7796341472e20df4246eabf0572a0b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-01 20:41:05 -0500

    Found tutorial for Xml parsing

---
+++ api-endpoint-tester/api-endpoint-tester.py
@@ -0,0 +1,7 @@
+# https://sdbrett.com/BrettsITBlog/2017/03/python-parsing-api-xml-response-data/
+
+import requests
+
+if __name__ == "__main__":
+    var_name = "Kristen"
+    print(f"Run {var_name}")
