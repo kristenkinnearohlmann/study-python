bbd57e0608d1858187fa961040e916b030d976f6
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-10-03 19:25:42 -0500

    Added another possible resource for XML project

--- api-endpoint-tester/api-endpoint-tester.py
+++ api-endpoint-tester/api-endpoint-tester.py
@@ -1,5 +1,6 @@
 # https://sdbrett.com/BrettsITBlog/2017/03/python-parsing-api-xml-response-data/
 # http://www.opengis.net/wfs
+# https://sv443.net/jokeapi/v2/joke/Any?format=xml
 
 import requests
 
